fb89de73038b9fee43e1ceceb12a87b05238a088
Alec Bossard <bsl4808a@ensfsi.onmicrosoft.com>
2025-01-09 08:21:55 +0100

    TP1 creation de la class Gaulois

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,16 @@
+package personnages;
+
+public class Gaulois {
+	protected String name;
+	protected int force;
+	
+	public Gaulois(String name, int force) {
+		this.name = name;
+		this.force = force;
+	}
+	
+	public String getName() {
+		return name ;
+	}
+	
+}
